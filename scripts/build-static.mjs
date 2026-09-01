86eabcf093cf804a6c8a737cf59eeab79c154bc8
Theodore Emeinidis <temeinidis@gmail.com>
2026-09-01 13:41:31 +0200

    Remove home service image frame lines

--- scripts/build-static.mjs
+++ scripts/build-static.mjs
@@ -34,7 +34,7 @@ await writeFile(
   <title>Η σελίδα δεν βρέθηκε | Eminidis Projects</title>
   <link rel="stylesheet" href="/styles.css?v=black9">
   <link rel="stylesheet" href="/responsive.css?v=black9">
-  <link rel="stylesheet" href="/pages.css?v=black54">
+  <link rel="stylesheet" href="/pages.css?v=black55">
 </head>
 <body>
   <main class="page-main">
